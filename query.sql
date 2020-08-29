35c5f766ba4a2e7774313bbfe32d7e746a2cab68
Liliana Ilut <lilianailut@Lilianas-MacBook-Air.local>
2020-08-29 13:23:58 -0500

    sql file

---
+++ query.sql
@@ -0,0 +1,13 @@
+CREATE TABLE happycovid (
+"Overall rank"  int,
+"Country or region" varchar not null,
+"Score" int,
+"GDP per capita" int,
+"Healthy life expectancy" int,
+"Total Cases" int,
+"Total Deaths" int,
+"Total Recovered" int,
+"Total Tests" int,
+"Population"int
+);
+select* from happycovid;
